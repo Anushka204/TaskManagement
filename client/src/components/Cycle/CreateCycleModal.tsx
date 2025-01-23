91724239be63179b2b331f4d65992f09f2802100
ihsavru <vurvashi11@gmail.com>
2025-01-24 02:25:48 +0530

    Update create cycle modal update desing and use shadcn calendar component for date range

--- client/src/components/Cycle/CreateCycleModal.tsx
+++ client/src/components/Cycle/CreateCycleModal.tsx
@@ -3,12 +3,26 @@ import useAuthToken from "../../hooks/useAuthToken"
 import { useNavigate } from "react-router-dom"
 import {
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
+  DialogFooter,
 } from "@/components/ui/dialog"
-import { useCycle } from "../../context/CycleContext"
-import { useToast } from "../../hooks/use-toast"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Separator } from "@/components/ui/separator"
+import { useCycle } from "@/context/CycleContext"
+import { useToast } from "@/hooks/use-toast"
+import { Textarea } from "@/components/ui/textarea"
+import { addDays, format } from "date-fns"
+import { CalendarIcon, Loader } from "lucide-react"
+import { DateRange } from "react-day-picker"
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 
 type Props = {}
 
@@ -16,12 +30,17 @@ const CreateCycleModal: React.FC<Props> = ({ setOpen }) => {
   const [newCycle, setNewCycle] = useState({
     title: "",
     description: "",
-    startDate: "",
-    endDate: "",
     visionBoardImage: "",
   })
 
+  const [date, setDate] = useState<DateRange | undefined>({
+    from: new Date(),
+    to: addDays(new Date(), 84),
+  })
+
   const [error, setError] = useState(null)
+  const [saving, setSaving] = useState(false)
+
   const { token, logout } = useAuthToken()
   const { addCycle } = useCycle()
 
@@ -30,13 +49,18 @@ const CreateCycleModal: React.FC<Props> = ({ setOpen }) => {
 
   const handleCreateCycle = async (e: MouseEvent) => {
     e.preventDefault()
+    setSaving(true)
     try {
       if (!token) {
         logout()
         navigate("/")
       }
 
-      await addCycle(newCycle)
+      await addCycle({
+        ...newCycle,
+        startDate: date.from,
+        endDate: date.to,
+      })
 
       toast({
         title: "Successfully created a new cycle!",
@@ -45,101 +69,133 @@ const CreateCycleModal: React.FC<Props> = ({ setOpen }) => {
       setNewCycle({
         title: "",
         description: "",
-        startDate: "",
-        endDate: "",
         visionBoardImage: "",
+      })
+
+      setDate({
+        from: new Date(),
+        to: addDays(new Date(), 84),
       })
     } catch (err) {
       setError(err.response?.data?.message || "Failed to create cycle")
     } finally {
       setOpen(false)
+      setSaving(false)
     }
   }
 
   return (
-    <DialogContent>
+    <DialogContent className='bg-neutral-900 border border-neutral-800 text-neutral-100 rounded-lg p-6'>
       <DialogHeader>
-        <DialogTitle>Create Cycle</DialogTitle>
-        <DialogDescription>
-          <div>
-            <form
-              onSubmit={handleCreateCycle}
-              className='flex flex-col gap-5 text-neutral-900'
-            >
-              <div>
-                <input
-                  placeholder='Title'
-                  className='bg-neutral-100 rounded-lg p-2 w-full'
-                  type='text'
-                  value={newCycle.title}
-                  onChange={(e) =>
-                    setNewCycle({ ...newCycle, title: e.target.value })
-                  }
-                  required
-                />
-              </div>
-              <div>
-                <textarea
-                  className='bg-neutral-100 rounded-lg p-2 w-full'
-                  placeholder='Description (Optional)'
-                  value={newCycle.description}
-                  onChange={(e) =>
-                    setNewCycle({ ...newCycle, description: e.target.value })
-                  }
-                />
-              </div>
-              <div>
-                <label>From:</label>
-                <input
-                  className='bg-neutral-100 rounded-lg p-2 w-full'
-                  type='date'
-                  value={newCycle.startDate}
-                  onChange={(e) =>
-                    setNewCycle({ ...newCycle, startDate: e.target.value })
-                  }
-                  required
-                />
-              </div>
-              <div>
-                <label>To:</label>
-                <input
-                  className='bg-neutral-100 rounded-lg p-2 w-full'
-                  type='date'
-                  value={newCycle.endDate}
-                  onChange={(e) =>
-                    setNewCycle({ ...newCycle, endDate: e.target.value })
-                  }
-                  required
-                />
-              </div>
-              <div>
-                <input
-                  placeholder='Vision Board Image URL'
-                  className='bg-neutral-100 rounded-lg p-2 w-full'
-                  type='text'
-                  value={newCycle.visionBoardImage}
-                  onChange={(e) =>
-                    setNewCycle({
-                      ...newCycle,
-                      visionBoardImage: e.target.value,
-                    })
-                  }
-                  required
-                />
-              </div>
-              <div className='flex justify-end'>
-                <button
-                  type='submit'
-                  className='mt-5 px-4 py-2 bg-blue-500 hover:bg-blue-600 hover:drop-shadow-lg transition-all text-white font-bold rounded-lg'
-                >
-                  Save
-                </button>
-                {error && <p style={{ color: "red" }}>{error}</p>}
-              </div>
-            </form>
-          </div>
-        </DialogDescription>
+        <DialogTitle className='text-lg text-lime-400 uppercase lexend-giga-700'>
+          Add new cycle
+        </DialogTitle>
+        <Separator className='my-3' />
       </DialogHeader>
+      <form
+        onSubmit={handleCreateCycle}
+        className='flex flex-col gap-5 text-neutral-900'
+      >
+        <div>
+          <Label className='text-xs text-neutral-400'>Title</Label>
+          <Input
+            placeholder='Cycle 1'
+            className='bg-neutral-800 mt-3 border-neutral-700 text-neutral-100 rounded-lg p-2 w-full'
+            type='text'
+            value={newCycle.title}
+            onChange={(e) =>
+              setNewCycle({ ...newCycle, title: e.target.value })
+            }
+            required
+          />
+        </div>
+        <div>
+          <Label className='text-xs text-neutral-400'>Description</Label>
+          <Textarea
+            className='bg-neutral-800 mt-3 border-neutral-700 text-neutral-100 rounded-lg p-2 w-full'
+            placeholder='Description (Optional)'
+            value={newCycle.description}
+            onChange={(e) =>
+              setNewCycle({ ...newCycle, description: e.target.value })
+            }
+          />
+        </div>
+        <div>
+          <Label className='text-xs text-neutral-400'>Cycle Duration</Label>
+          <Popover modal={true}>
+            <PopoverTrigger asChild>
+              <Button
+                id='date'
+                variant={"outline"}
+                className='bg-neutral-800 mt-3 border-neutral-700 text-neutral-100 rounded-lg p-2 w-full'
+              >
+                <CalendarIcon />
+                {date?.from ? (
+                  date?.to ? (
+                    <>
+                      {format(date?.from, "LLL dd, y")} -{" "}
+                      {format(date?.to, "LLL dd, y")}
+                    </>
+                  ) : (
+                    format(date?.from, "LLL dd, y")
+                  )
+                ) : (
+                  <span>Pick a date range</span>
+                )}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className='w-auto p-0' align='start'>
+              <Calendar
+                initialFocus
+                mode='range'
+                defaultMonth={date?.from}
+                selected={date}
+                onSelect={setDate}
+                numberOfMonths={2}
+              />
+            </PopoverContent>
+          </Popover>
+        </div>
+        <div>
+          <Label className='text-xs text-neutral-400'>Vision Board</Label>
+          <Input
+            placeholder='Enter the URL to your image: https//example.com/image.jpg'
+            className='bg-neutral-800 mt-3 border-neutral-700 text-neutral-100 rounded-lg p-2 w-full'
+            type='text'
+            value={newCycle.visionBoardImage}
+            onChange={(e) =>
+              setNewCycle({
+                ...newCycle,
+                visionBoardImage: e.target.value,
+              })
+            }
+            required
+          />
+        </div>
+        <DialogFooter>
+          <div className='flex justify-end'>
+            <button
+              type='submit'
+              className={`text-sm px-4 py-2 ${
+                saving ? "bg-neutral-400" : "bg-lime-400"
+              } text-neutral-900 lexend-giga-700 uppercase font-bold rounded-lg`}
+            >
+              {saving ? (
+                <div className="flex items-center gap-2">
+                  <Loader
+                    size={15}
+                    className='animate-[spin_2s_linear_infinite]'
+                  />
+                  <span>Saving</span>
+                </div>
+              ) : (
+                "Save"
+              )}
+            </button>
+            {error && <p className="text-red-500">{error}</p>}
+          </div>
+        </DialogFooter>
+      </form>
     </DialogContent>
   )
 }
